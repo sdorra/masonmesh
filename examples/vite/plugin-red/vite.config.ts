d3464cc314bbcb56e662008cbe21a5a60c107682
Sebastian Sdorra <s.sdorra@gmail.com>
2023-09-21 13:13:32 +0200

    Suppress eslint warning

--- examples/vite/plugin-red/vite.config.ts
+++ examples/vite/plugin-red/vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
 		lib: {
 			entry: path.resolve(__dirname, "src/index.ts"),
 			name: "plugin-red",
+			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 			// @ts-ignore i don't know why vite does not export a type for amd
 			formats: ["amd"],
 		},
